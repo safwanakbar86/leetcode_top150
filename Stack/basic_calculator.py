076e3a44a80ae934664c17fdaf60c5ed8987c702
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-03-09 16:45:36 +0500

    Edited basic_calculator.py

--- Stack/basic_calculator.py
+++ Stack/basic_calculator.py
@@ -1,40 +1,50 @@
 #INCOMPLETE
 
 def basic_calc(s):
-    return basic_calc_recursive(s, [])
-
-def basic_calc_recursive(s, stack):
-    answer = 0
-    for c in range(len(s)):
-        if c == ' ':
-            continue
-        elif stack[-1] == '+':
-            if c == '(':
-                answer += basic_calc_recursive(s[c+1:], ['('])
-            else:
-                answer += int(c)
-                stack.pop()
-        elif stack[-1] == '-':
-            if c == '(':
-                answer -= basic_calc_recursive(s[c+1:], ['('])
-            else:
-                answer -= int(c)
-                stack.pop()
-        elif c == '(':
-            answer += basic_calc_recursive(s[c+1:], ['('])
-        elif c == ')':
-            stack.pop()
-            return answer
-        else:
-            if c != '+' or c != '-':
-                answer += c
-            else:
-                stack.append(c)
+    answer, index = basic_calc_recursive(s, ['$'])
     return answer
 
-s = "   1 + 1 "
-#s = "  2-1 + 2 "
-#s = "(1+(4+5+2)-3)+(6+8)"
+def basic_calc_recursive(s, stack):
+    i = 0
+    answer = 0
+    while i < len(s):
+        if s[i] == ' ':
+            i += 1
+            continue
+        elif stack[-1] == '+':
+            if s[i] == '(':
+                temp, index = basic_calc_recursive(s[i+1:], ['$', '('])
+                answer += temp
+                i += index
+            else:
+                answer += int(s[i])
+                i += 1
+            stack.pop()
+        elif stack[-1] == '-':
+            if s[i] == '(':
+                temp, index = basic_calc_recursive(s[i+1:], ['$', '('])
+                answer -= temp
+                i += index
+            else:
+                answer -= int(s[i])
+                i += 1
+            stack.pop()
+        elif s[i] == '(':
+            temp, index = basic_calc_recursive(s[i+1:], ['$', '('])
+            answer += temp
+            i += index
+        elif s[i] == ')':
+            stack.pop()
+            return answer, i+2
+        else:
+            if s[i] != '+' and s[i] != '-':
+                answer += int(s[i])
+            else:
+                stack.append(s[i])
+            i += 1
+    return answer, 0
+
+s = "(1+(4+5+2)-3)+(6+8)"
 s = s.strip()
 answer = basic_calc(s)
 print(answer)
