e785e38f6ebbf2fd7621d45d9420a11a3ab1ae48
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-03-11 18:41:26 +0500

    Update two_sum.py

--- Hashmap/two_sum.py
+++ Hashmap/two_sum.py
@@ -1,5 +1,8 @@
+#BRUTE FORCE APPROACH
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+
+#HASHMAP APPROACH
