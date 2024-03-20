ae4685b3032d1ba8e3beca058a1c9c4fe207811e
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-03-21 01:02:56 +0500

    Updated two_sum.py

--- Hashmap/two_sum.py
+++ Hashmap/two_sum.py
@@ -1,8 +1,20 @@
-#BRUTE FORCE APPROACH
+#BRUTE FORCE APPROACH O(N^2)
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
 
-#HASHMAP APPROACH
+#HASHMAP APPROACH O(N)
+    def twoSum(self, nums, target):
+        return_list = []
+        c = 1
+        
+        for n in range(len(nums) - 1):
+            temp_nums = nums[n+1:]
+            if target - nums[n] in temp_nums:
+                return_list.append(n)
+                return_list.append(temp_nums.index(target - nums[n]) + c)
+            c += 1
+        
+        return return_list
