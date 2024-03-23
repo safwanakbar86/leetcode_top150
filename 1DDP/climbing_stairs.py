aa7f5f219c9ffdd0209c6b08fbf740cf98616971
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-03-23 22:48:56 +0500

    Update climbing_stairs.py

--- 1DDP/climbing_stairs.py
+++ 1DDP/climbing_stairs.py
@@ -1,1 +1,12 @@
-# IN PROGRESS
+    def climbStairs(self, n):
+        if n < 0:
+            return 0
+        elif n <= 1:
+            return n
+        else:
+            memo = []
+            memo.append(0)
+            memo.append(1)
+            for i in range(2, n+2):
+                memo.append(memo[i-1] + memo[i-2])
+            return memo[-1]
