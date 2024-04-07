75a749b74543ab8b346cfd0c5b99dc8370a478b8
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-04-07 23:13:47 +0500

    Update product_of_array_except_self.py

--- ArrayString/product_of_array_except_self.py
+++ ArrayString/product_of_array_except_self.py
@@ -1,1 +1,15 @@
-
+  def productExceptSelf(self, nums):
+      length = len(nums)
+      result = 1
+      answer =[1] * length
+      
+      for index in range(length):
+          answer[index] = result
+          result *= nums[index]
+          
+      result = 1
+      for index in reversed(range(length)):
+          answer[index] *= result
+          result *= nums[index]
+          
+      return answer
