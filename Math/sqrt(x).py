3ef6725b83199d8c7eb8611c0c5a1d96891c4706
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-04-17 19:27:49 +0500

    Update sqrt(x).py

--- Math/sqrt(x).py
+++ Math/sqrt(x).py
@@ -1,5 +1,13 @@
 def mySqrt(self, x):
-    n = 0
-    while n * n <= x:
-        n += 1
-    return n - 1
+    left = 0
+    right = x
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if mid * mid < x:
+            left = mid + 1
+        elif mid * mid > x:
+            right = mid - 1
+        else:
+            return mid
+    return right
