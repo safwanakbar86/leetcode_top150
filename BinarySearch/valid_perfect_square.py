2ce893e635c9f50e1402f935480a1024121c91f3
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-04-18 16:55:52 +0500

    Create valid_perfect_square.py

---
+++ BinarySearch/valid_perfect_square.py
@@ -0,0 +1,14 @@
+def isPerfectSquare(self, num):
+    left = 0
+    right = num
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if mid * mid < num:
+            left = mid + 1
+        elif mid * mid > num:
+            right = mid - 1
+        else:
+            return True
+    return False
+  
